ddd8ae009929c0b4275462a929eb2bbb0ce41e99
CesarxArteaga <ctmces94@gmail.com>
2020-08-27 17:14:14 -0500

    deploy

---
+++ precache-manifest.de4161669a765153d8f3833ca0c8102f.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5125a8736178fdf0026",
+    "url": "/loly/css/about.95ae2909.css"
+  },
+  {
+    "revision": "4cb22d23854926aa3197",
+    "url": "/loly/css/app.31cdae90.css"
+  },
+  {
+    "revision": "fb360ee8fe2ea054a064",
+    "url": "/loly/css/chunk-vendors.14427ded.css"
+  },
+  {
+    "revision": "f6bc6bbf4e86c994f345bcb1d0f9d643",
+    "url": "/loly/img/AYUDA.f6bc6bbf.png"
+  },
+  {
+    "revision": "1786f3099042f5f65d5f86587e6e358c",
+    "url": "/loly/img/BONITA.1786f309.png"
+  },
+  {
+    "revision": "1cf44b4a22797e34fcf1f02b6a0df605",
+    "url": "/loly/img/CUANDO.1cf44b4a.png"
+  },
+  {
+    "revision": "8816a2b0dde5fc1ab1fe65ee5a746b1f",
+    "url": "/loly/img/DIJERON.8816a2b0.png"
+  },
+  {
+    "revision": "4e16170df08ad635c7880df067e41489",
+    "url": "/loly/img/DOS_DEDO.4e16170d.png"
+  },
+  {
+    "revision": "8ec206bc0b11aa69b90a57054aeee153",
+    "url": "/loly/img/ELLA.8ec206bc.png"
+  },
+  {
+    "revision": "5df6e6b23983a399e75578263ac3d128",
+    "url": "/loly/img/ESTABA.5df6e6b2.png"
+  },
+  {
+    "revision": "710b70e7b917d27ddedf8e3d5d70947e",
+    "url": "/loly/img/ESTA_ACNT.710b70e7.png"
+  },
+  {
+    "revision": "560f7b1cd4de9bce987c3c238536d182",
+    "url": "/loly/img/EXITO.560f7b1c.png"
+  },
+  {
+    "revision": "9139135dfa4f79d36fab2abef3c993ef",
+    "url": "/loly/img/GABY.9139135d.png"
+  },
+  {
+    "revision": "74bbc93ccf5540b0c44ec164a9543e79",
+    "url": "/loly/img/HABLAR_GABY.74bbc93c.png"
+  },
+  {
+    "revision": "9bb8c6f69e14ee14e1ca49e7ea97e05d",
+    "url": "/loly/img/HABLAR_KIKE.9bb8c6f6.png"
+  },
+  {
+    "revision": "74699d63c0a8121c3a2b04e263981711",
+    "url": "/loly/img/HACE_LLEGAR.74699d63.png"
+  },
+  {
+    "revision": "6d545ff871ba6757484bfb1052f311c3",
+    "url": "/loly/img/HURRA.6d545ff8.png"
+  },
+  {
+    "revision": "674f554293a7f755c86c77b4b006ceed",
+    "url": "/loly/img/INVENTO.674f5542.png"
+  },
+  {
+    "revision": "9bf644e6bfee4718d272b568d9d58d12",
+    "url": "/loly/img/KIKE.9bf644e6.png"
+  },
+  {
+    "revision": "1be04362c720e5c9be0c060f62657772",
+    "url": "/loly/img/MARAVILLOSA_IDEA.1be04362.png"
+  },
+  {
+    "revision": "36121ca600f69d179610698feddd4e6f",
+    "url": "/loly/img/NO.36121ca6.png"
+  },
+  {
+    "revision": "b24d0f10207a36322691af3ab1815c96",
+    "url": "/loly/img/PADRES.b24d0f10.png"
+  },
+  {
+    "revision": "b71a66d114f924a899e95b1b82d054eb",
+    "url": "/loly/img/PARQUE.b71a66d1.png"
+  },
+  {
+    "revision": "4a553ab00dcdb1e7f3128363dd0a202e",
+    "url": "/loly/img/PINTAR.4a553ab0.png"
+  },
+  {
+    "revision": "ddac43c624537c3a4b29f99f91c9cca4",
+    "url": "/loly/img/PODER.ddac43c6.png"
+  },
+  {
+    "revision": "352ffcf1152f891c25ccab43004bc669",
+    "url": "/loly/img/PODIA.352ffcf1.png"
+  },
+  {
+    "revision": "fcb3e66793416f4e4afaaab2193feefb",
+    "url": "/loly/img/PODRAN.fcb3e667.png"
+  },
+  {
+    "revision": "beeaee307bee5d7abff1042ba5794f06",
+    "url": "/loly/img/PODRIA.beeaee30.png"
+  },
+  {
+    "revision": "91c6c2306ed004f31bb1463ef03d69cd",
+    "url": "/loly/img/PREOCUPADOS.91c6c230.png"
+  },
+  {
+    "revision": "ebdd0ca8de0ef64ea5590b38afe4d6d4",
+    "url": "/loly/img/PUDO.ebdd0ca8.png"
+  },
+  {
+    "revision": "d86874f78ed434a652ae08912e107099",
+    "url": "/loly/img/SI.d86874f7.png"
+  },
+  {
+    "revision": "a6fc61d958928c12502c5c9c5f1580fb",
+    "url": "/loly/img/SUS_KIKE.a6fc61d9.png"
+  },
+  {
+    "revision": "223725045714e6c2a10ec37a7bc587e9",
+    "url": "/loly/img/SU_KIKE.22372504.png"
+  },
+  {
+    "revision": "94db8ad7da128d3b423ed6c1a7ae0f96",
+    "url": "/loly/img/TODAS.94db8ad7.png"
+  },
+  {
+    "revision": "1c29c58c54ba4581a913c970708b7ad1",
+    "url": "/loly/img/TODO.1c29c58c.png"
+  },
+  {
+    "revision": "0ff14074a10edbcb789d2169b7bc395b",
+    "url": "/loly/img/TRISTE.0ff14074.png"
+  },
+  {
+    "revision": "13cd00617a8ebf353b752cde2b667274",
+    "url": "/loly/img/VENCIDO.13cd0061.png"
+  },
+  {
+    "revision": "bf5ecd81569e3233c313225e98d0523d",
+    "url": "/loly/img/VIDEOJUEGOS.bf5ecd81.png"
+  },
+  {
+    "revision": "5d06aac15556860024dd5e26f121bb93",
+    "url": "/loly/img/VIRUS_MALVADO.5d06aac1.png"
+  },
+  {
+    "revision": "423d196b45556ccf4ced0aef2e0dd3a9",
+    "url": "/loly/img/audio_button.423d196b.png"
+  },
+  {
+    "revision": "dbf8657b831117acabbb23ec287578f3",
+    "url": "/loly/img/close_button.dbf8657b.png"
+  },
+  {
+    "revision": "1fae84d6cb3fa029b2f183eaa731658c",
+    "url": "/loly/img/cover.1fae84d6.jpg"
+  },
+  {
+    "revision": "ff5d7b1de9f1464ce30256707ad7dd5d",
+    "url": "/loly/img/creditos_button.ff5d7b1d.png"
+  },
+  {
+    "revision": "9ba9e20ac7d2423bac08af161b1605e3",
+    "url": "/loly/img/home_button.9ba9e20a.png"
+  },
+  {
+    "revision": "51eb3c3a9ace68957098c22dd565b462",
+    "url": "/loly/img/horizontal.51eb3c3a.png"
+  },
+  {
+    "revision": "985667b4148d4f0bc8fedbcac5db9d3b",
+    "url": "/loly/img/pasta.985667b4.png"
+  },
+  {
+    "revision": "93adbe3722c91bc075867641342564c6",
+    "url": "/loly/img/scene_1.93adbe37.jpg"
+  },
+  {
+    "revision": "87d64045c78fcce40b15964a3a389e61",
+    "url": "/loly/img/scene_10.87d64045.jpg"
+  },
+  {
+    "revision": "3d7a618360c085df2e608d105d2ebeab",
+    "url": "/loly/img/scene_11.3d7a6183.jpg"
+  },
+  {
+    "revision": "720f095bfb8fdb7bf6b3f0064e086f84",
+    "url": "/loly/img/scene_12.720f095b.jpg"
+  },
+  {
+    "revision": "6b68b4b9cdc570f9ddaed99a3f0cdbf9",
+    "url": "/loly/img/scene_13.6b68b4b9.jpg"
+  },
+  {
+    "revision": "b06381d027a7e901ee1ed132b80d6e26",
+    "url": "/loly/img/scene_14.b06381d0.jpg"
+  },
+  {
+    "revision": "33b1c1fa83defc51f0f3aaa827e52f54",
+    "url": "/loly/img/scene_2.33b1c1fa.jpg"
+  },
+  {
+    "revision": "02dc5bc1c97718a150ea02fc2bfd1f01",
+    "url": "/loly/img/scene_3.02dc5bc1.jpg"
+  },
+  {
+    "revision": "c5cdd01b968b831ea1b79a21e57c74e8",
+    "url": "/loly/img/scene_4.c5cdd01b.jpg"
+  },
+  {
+    "revision": "2e13864ba569b532287221bf10ffcf2a",
+    "url": "/loly/img/scene_5.2e13864b.jpg"
+  },
+  {
+    "revision": "e972a875f2606388f8ed3f12bdb6e526",
+    "url": "/loly/img/scene_7.e972a875.jpg"
+  },
+  {
+    "revision": "ffb5b9506a80fb9fdbb1506f2a97ea81",
+    "url": "/loly/img/scene_8.ffb5b950.jpg"
+  },
+  {
+    "revision": "5be095b5a16f5ba75f78ed8f7969b555",
+    "url": "/loly/img/scene_9.5be095b5.jpg"
+  },
+  {
+    "revision": "0b151875cd1e78af221b4cb0030059d5",
+    "url": "/loly/index.html"
+  },
+  {
+    "revision": "d5125a8736178fdf0026",
+    "url": "/loly/js/about.7c88d1d9.js"
+  },
+  {
+    "revision": "4cb22d23854926aa3197",
+    "url": "/loly/js/app.86f2376b.js"
+  },
+  {
+    "revision": "fb360ee8fe2ea054a064",
+    "url": "/loly/js/chunk-vendors.7112f99c.js"
+  },
+  {
+    "revision": "b063d89ea9393ba59a89d61c60d7880c",
+    "url": "/loly/manifest.json"
+  },
+  {
+    "revision": "1219fdc9f60c2d38d1dfe263d5901471",
+    "url": "/loly/media/scene_1.1219fdc9.mp3"
+  },
+  {
+    "revision": "376858cd2766a73569566e1df9d5e46d",
+    "url": "/loly/media/scene_10.376858cd.mp3"
+  },
+  {
+    "revision": "48bee0d6aecb36e29a29ee4304c65206",
+    "url": "/loly/media/scene_11.48bee0d6.mp3"
+  },
+  {
+    "revision": "b18c20de6a42da9148404c53d6c644ac",
+    "url": "/loly/media/scene_12.b18c20de.mp3"
+  },
+  {
+    "revision": "ac35ffb17c983cfe6974902da8ede2cb",
+    "url": "/loly/media/scene_13.ac35ffb1.mp3"
+  },
+  {
+    "revision": "6dc0f8cf43e5a9635a820fe03b9c4e41",
+    "url": "/loly/media/scene_14.6dc0f8cf.mp3"
+  },
+  {
+    "revision": "d973c3eeca9b791da684d443de2fa1d2",
+    "url": "/loly/media/scene_2.d973c3ee.mp3"
+  },
+  {
+    "revision": "2053114abac991dc650a6db5d6ffca90",
+    "url": "/loly/media/scene_3.2053114a.mp3"
+  },
+  {
+    "revision": "0f0288b127d47d06ab6416e9f774ce4b",
+    "url": "/loly/media/scene_4.0f0288b1.mp3"
+  },
+  {
+    "revision": "4059852c39a4b7d64f87720679486ae4",
+    "url": "/loly/media/scene_5.4059852c.mp3"
+  },
+  {
+    "revision": "c86737f8e650f2eb5d2bf46434806c80",
+    "url": "/loly/media/scene_6.c86737f8.mp3"
+  },
+  {
+    "revision": "80c0c28c28cb3c1345227bdfe8ee61f5",
+    "url": "/loly/media/scene_7.80c0c28c.mp3"
+  },
+  {
+    "revision": "4daf4f8bb3d3bab75f9d28f503b6e470",
+    "url": "/loly/media/scene_8.4daf4f8b.mp3"
+  },
+  {
+    "revision": "998caa7567743f1f8d937eed7e6cc806",
+    "url": "/loly/media/scene_9.998caa75.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/loly/robots.txt"
+  }
+]);
